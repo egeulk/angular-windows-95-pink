bb762363492ba0ab0ecbe2bc3c1b5cb375ec9326
egeulk <egeulker99@gmail.com>
2023-01-19 10:25:47 +0300

    small fix, new demo image

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { RegisterComponent } from '../register/register.component';
+import { WindowsService } from '../windows.service';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor(
+    private readonly windowsService: WindowsService,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  openRegister(): void {
+    this.windowsService.openMenuItem({
+      name: '📄 Register',
+      goTo: RegisterComponent,
+      image: 'https://win98icons.alexmeub.com/icons/png/ac_plug-0.png'
+    });
+  }
+
+}
